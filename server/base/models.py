dc330347dacc5b5288d27d20bdc375fee9cc3f1f
shket-228 <daniildudov@gmail.com>
2022-07-24 11:23:22 +0300

    Todo added

--- server/base/models.py
+++ server/base/models.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    # TODO str id translit
     name = models.CharField(max_length=256, primary_key=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
+    # TODO str id translit
+    name = models.CharField(max_length=256, primary_key=True)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
---
+++ server/dbinit.sql
@@ -0,0 +1,5 @@
+DELETE FROM base_product
+WHERE id = "Nice bag";
+
+INSERT INTO base_product
+VALUES ('Nice bag', 'Really nice', 228.00, 'nicebag.jpg', TRUE, 'Мешки');
