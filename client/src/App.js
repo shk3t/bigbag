962df2384a96e4b539a0793e766d19a8c7c27cb2
EkaterinaVikt <kat.abramova@gmail.com>
2022-07-28 19:23:36 +0300

    Add About, Deliv, Cart
    Пока без компонентов

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
 import MainPage from "./pages/MainPage";
 import CatalogPage from "./pages/CatalogPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import DeliveryPage from "./pages/DeliveryPage";
+import MyCartPage from "./pages/MyCartPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
+          {/* путь константами */}
           <Route path="/" element={<MainPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/products/:id" element={<ProductPage />} />
           <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/delivery" element={<DeliveryPage />} />
+          <Route path="/cart" element={<MyCartPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ client/src/pages/MyCartPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Way from "../components/Way";
+import Footer from "../components/Footer";
+import "../styles/main.css";
+
+export default function MyCartPage() {
+  return (
+    <div>
+      <Header />
+      <Way />
+      <Footer />
+    </div>
+  );
+}
