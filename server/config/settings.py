e675b7c5cf04fcb33022341f2bb6cc91f07f786a
shket-228 <daniildudov@gmail.com>
2022-08-22 18:16:14 +0300

    localhost alias CORS fix

--- server/config/settings.py
+++ server/config/settings.py
@@ -72,8 +72,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CORS
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3228",
     "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3228",
+    "http://localhost:3228",
 ]
 
 # REST framework extensions
