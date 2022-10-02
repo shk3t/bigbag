0199d9eebd1d7d25965614490897ba7962ca0515
shket-228 <daniildudov@gmail.com>
2022-10-02 11:51:07 +0300

    случайный баг

--- client/src/utils/errors.js
+++ client/src/utils/errors.js
@@ -9,7 +9,7 @@ export function extractErrorMessages(error) {
   const messages = [];
 
 
-  function translate() {
+  function translate(word) {
     const translation = TRANSLATIONS[word.toLowerCase()];
     return translation || word;
   }
